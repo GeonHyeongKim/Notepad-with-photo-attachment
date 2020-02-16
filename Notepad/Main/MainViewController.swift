129022ed80ae4cb79bef9a6d80288e78498f7bcb
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-02-16 22:54:58 +0900

    fix : MainViewController로 변경
    Note의 List를 관리하는 기능

---
+++ Notepad/Main/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  Notepad
+//
+//  Created by gunhyeong on 2020/02/16.
+//  Copyright © 2020 geonhyeong. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
