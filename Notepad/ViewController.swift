669654daaae434b53747b4d0fc6011d311f23193
GeonHyeong Kim <geonhyeong.dev@gmail.com>
2020-02-16 21:42:11 +0900

    Initial Commit

---
+++ Notepad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notepad
+//
+//  Created by gunhyeong on 2020/02/16.
+//  Copyright © 2020 geonhyeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
